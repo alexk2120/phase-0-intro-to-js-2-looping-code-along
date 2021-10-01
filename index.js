1ab0888132741fd56c86932462c512b7e12e8b67
alexander.Khatamov <alexk2120@gmail.com>
2021-10-01 17:06:35 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    for (let i = 0; i < 3; i++) {
+        console.log(`Thank you ${names[i]}, for the wonderful ${event} gift!`);
+       
+                
+            } 
+        
+        }
+     writeCards([ "Ada", "Brendan", "Ali"], "surprise")
+    
