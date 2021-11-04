3c8a1eeda6862416f91f20ef2d3bf1e375ff8a6f
alexander.Khatamov <alexk2120@gmail.com>
2021-11-03 22:38:03 -0400

    commit tree 56426cba91a815050ae528871a153f094ad5e144

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-function writeCards(names, event) {
-    for (let i = 0; i < 3; i++) {
-        console.log(`Thank you ${names[i]}, for the wonderful ${event} gift!`);
-       
-                
-            } 
-        
-        }
-     writeCards([ "Ada", "Brendan", "Ali"], "surprise")
-    
+function writeCards(array, event) {
+    const cards = [];
+      for (let i = 0; i < array.length; i++) {
+          
+          cards.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+      }
+      return cards;
+  }
+  writeCards(["Lisa", "Kaitlin", 'Jan'], "surprise");
+  
+ function countDown(i){
+     while (i >= 0) {
+         console.log(i);   
+         i--;
+     }
+ }
